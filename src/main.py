c819c0909c2c36cd41066cb2fa613167fb8b49ce
Matin Karbasioun <m.karbasion@irfarabi.com>
2024-10-06 09:28:26 +0330

    [Refactor] refactor packaging and structures

--- src/main.py
+++
@@ -1,8 +0,0 @@
-from src.Application.agent.agent import Agent
-from src.Application.environment.env import Environment
-
-
-if __name__ == '__main__':
-    env = Environment('FrozenLake-v1')
-    agent = Agent(env)
-    agent.train()
